831866a057399caf444c3be06f3df3281ab8969d
Abel Molina Zamorano <Abel Molina Zamorano@ABELPC>
2025-03-06 18:09:11 +0100

    Agregar archivos de proyecto.

---
+++ PracticaCV_AbelMZ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_AbelMZ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
